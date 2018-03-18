90affecf5d435b8b647bd6e61de890cf122735f6
Stefan Church <stefan.church@gmail.com>
2018-03-18 17:55:41 +1300

    Add hindent support

--- init.el
+++ init.el
@@ -97,6 +97,9 @@
 (add-hook 'after-save-hook #'update-haskell-tags)
 (setq tags-revert-without-query 1)
 
+(require 'hindent)
+(add-hook 'haskell-mode-hook #'hindent-mode)
+
 ;; Flycheck
 (add-hook 'after-init-hook 'global-flycheck-mode)
 (eval-after-load 'flycheck
@@ -136,7 +139,6 @@
 ;; Dockerfile
 (require 'dockerfile-mode)
 (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
-
 (require 'docker-compose-mode)
 
 (custom-set-variables
@@ -149,7 +151,7 @@
     ("65a2801c39211a69af6cc4c8441f3f51871404b9d12a6f2966a7bc33468995c7" "ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" default)))
  '(package-selected-packages
    (quote
-    (docker-compose-mode dockerfile-mode docker emmet-mode impatient-mode skewer-mode web-mode drag-stuff doom-themes dracula-theme paredit multi-web-mode js2-mode idris-mode robe projectile-rails helm exec-path-from-shell projectile yaml-mode magit flycheck-elm elm-mode intero company flycheck))))
+    (hindent docker-compose-mode dockerfile-mode docker emmet-mode impatient-mode skewer-mode web-mode drag-stuff doom-themes dracula-theme paredit multi-web-mode js2-mode idris-mode robe projectile-rails helm exec-path-from-shell projectile yaml-mode magit flycheck-elm elm-mode intero company flycheck))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
