aea2c5f981b9dab9ed5d61dc349511e4a51b3f61
Stefan Church <stefan.church@gmail.com>
2018-03-27 20:07:50 +1300

    Add darcula theme

---
+++ elpa/sunburn-theme-20180317.1051/sunburn-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "sunburn-theme" "20180317.1051" "A low contrast color theme" '((emacs "24")) :commit "63fbc5a609100e78d8304c99646ae8f689e17bf2" :url "http://github.com/mvarela/Sunburn-Theme")
--- init.el
+++ init.el
@@ -146,12 +146,41 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
  '(custom-safe-themes
    (quote
-    ("65a2801c39211a69af6cc4c8441f3f51871404b9d12a6f2966a7bc33468995c7" "ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" default)))
+    ("6fb46156a89a9d1506d0acf7e904eabfaafccd3d95208cf43fac891918b7e3fb" "65a2801c39211a69af6cc4c8441f3f51871404b9d12a6f2966a7bc33468995c7" "ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" default)))
+ '(fci-rule-color "#383838")
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(package-selected-packages
    (quote
-    (hindent docker-compose-mode dockerfile-mode docker emmet-mode impatient-mode skewer-mode web-mode drag-stuff doom-themes dracula-theme paredit multi-web-mode js2-mode idris-mode robe projectile-rails helm exec-path-from-shell projectile yaml-mode magit flycheck-elm elm-mode intero company flycheck))))
+    (darcula-theme sunburn-theme hindent docker-compose-mode dockerfile-mode docker emmet-mode impatient-mode skewer-mode web-mode drag-stuff doom-themes dracula-theme paredit multi-web-mode js2-mode idris-mode robe projectile-rails helm exec-path-from-shell projectile yaml-mode magit flycheck-elm elm-mode intero company flycheck)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
